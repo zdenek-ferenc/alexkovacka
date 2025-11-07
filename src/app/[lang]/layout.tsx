496a65c05591ecec4aa2947d33d122b08a3b4f0d
zdenekFerenc <zdenekk.ferenc@gmail.com>
2025-11-07 13:47:42 +0100

    FIX: Oprava Lang params

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -6,6 +6,10 @@ type LangLayoutParams = {
   params: { lang: 'cs' | 'en' };
 };
 
+export async function generateStaticParams() {
+  return [{ lang: 'cs' }, { lang: 'en' }];
+}
+
 export default async function LangLayout({ children, params: { lang } }: LangLayoutParams) {
   
   const dictionary = await getDictionary(lang);
